f6f8780771d32efef19debb480cbc947d79bbc88
Adrian Garcia Rios <agarciar@GMU.EDU>
2024-05-24 17:57:16 -0400

    fixes

--- src/component/MenuButton.tsx
+++ src/component/MenuButton.tsx
@@ -3,11 +3,17 @@
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
+import React, { MouseEventHandler } from "react";
 
-const MenuButton = ({ isOpen, toggle }) => {
+interface MenuProps {
+    isOpen: boolean,
+    toggle: (menuItem: MouseEventHandler) => void,
+}
+
+const MenuButton: React.FC<MenuProps> = ({ isOpen, toggle }) => {
     return (
         <motion.button
-            onClick={toggle}
+            onClick={() => toggle}
             className=" fixed top-0 left-0 z-50 p-4 flex items-center bg-white rounded-full ml-1"
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
