6857693222f78935733b883808707bf9e0b6f2c8
Adrian Garcia Rios <agarciar@GMU.EDU>
2024-08-06 15:51:04 -0400

    added social links

--- src/component/AboutMe.tsx
+++ src/component/AboutMe.tsx
@@ -1,4 +1,7 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import Link from "next/link";
 import snapchatpic from "../../public/images/snapchatpic.jpg";
 
 export default function AboutMe() {
@@ -10,8 +13,12 @@ export default function AboutMe() {
                     <p>My Name is Adrian and I am a Full Stack Developer.</p>
                     <p>I also have experience working in the Telecommunicatons field.</p>
                     <p>Curently working on a Blog page.</p>
+                    <div className="text-white flex gap-2">
+                        <Link href={"https://github.com/ako1player"} target="_blank"><FontAwesomeIcon icon={faGithub} size="2xl" /></Link>
+                        <Link href={"https://www.linkedin.com/in/adriangarciarios/"} target="_blank"><FontAwesomeIcon icon={faLinkedin} size="2xl" /></Link>
+                    </div>
                 </div>
-                <div className="">
+                <div>
                     <Image src={snapchatpic} alt="Adrian SnapChat Pic" width={400} height={400} className=" rounded-full" priority />
                 </div>
             </div>
